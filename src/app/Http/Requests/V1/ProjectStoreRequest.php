0284fd513b57b40c59647f804467840e058c41ee
Artem <alexx1088@gmail.com>
2022-09-16 12:20:50 +0600

    correction code in model Add_project

--- src/app/Http/Requests/V1/ProjectStoreRequest.php
+++ src/app/Http/Requests/V1/ProjectStoreRequest.php
@@ -13,7 +13,7 @@ class ProjectStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,29 @@ class ProjectStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            're-examination',
+            'incoming_letter_number',
+            'outgoing_letter_number',
+            'date_of_outgoing_letter',
+            'Date_of_receipt_project_legal act',
+            'document_type',
+            'project_act_name_rus',
+            'project_act_name_kaz',
+            'branch of legislation',
+            'purpose_legal_act_rus',
+            'documents_submitted_for_expertise_rus',
+            'documents_submitted_for_expertise_kaz',
+            'subject_and_goals_expertise_rus',
+            'subject_and_goals_expertise_kaz',
+            'number_of_pages',
+            'dev_group',
+            'email',
+            'comment',
+            'full_name_rus',
+            'full_name_kaz',
+            'job_title_rus',
+            'job_title_kz',
+            'phone',
         ];
     }
 }
