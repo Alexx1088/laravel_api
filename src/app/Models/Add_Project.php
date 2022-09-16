aa4bbfbf579af415b4595fe0196e62aa82f4b2a8
Artem <alexx1088@gmail.com>
2022-09-16 11:32:03 +0600

    add migration 'add_project' and model

---
+++ src/app/Models/Add_Project.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Add_Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        're-examination',
+        'incoming_letter_number',
+        'outgoing_letter_number',
+        'outgoing_letter_number',
+        'Date_of_receipt_project_legal act',
+        'document_type',
+        'project_act_name_rus',
+        'project_act_name_kaz',
+        'branch of legislation',
+        'purpose_legal_act_rus',
+        'documents_submitted_for_expertise_rus',
+        'documents_submitted_for_expertise_rus',
+        'subject_and_goals_expertise_rus',
+        'subject_and_goals_expertise_kaz',
+        'number_of_pages',
+        'dev_group',
+        'email',
+        'comment',
+        'full_name_rus',
+        'full_name_kaz',
+        'job_title_rus',
+        'job_title_kz',
+        'phone',
+    ];
+}
---
+++ src/database/migrations/2022_09_16_040121_create_add__projects_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('add__projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('re-examination');
+            $table->integer('incoming_letter_number');
+            $table->integer('outgoing_letter_number');
+            $table->dateTime('date_of_outgoing_letter');
+            $table->dateTime('date_of_receipt_project_legal act');
+            $table->string('document_type');
+            $table->string('project_act_name_rus');
+            $table->string('project_act_name_kaz');
+            $table->string('branch of legislation');
+            $table->string('purpose_legal_act_rus');
+            $table->string('documents_submitted_for_expertise_rus');
+            $table->string('documents_submitted_for_expertise_kaz');
+            $table->string('subject_and_goals_expertise_rus');
+            $table->string('subject_and_goals_expertise_kaz');
+            $table->integer('number_of_pages');
+            $table->string('dev_group');
+            $table->string('email')->unique();
+            $table->string('comment');
+            $table->string('full_name_rus');
+            $table->string('full_name_kaz');
+            $table->string('job_title_rus');
+            $table->string('job_title_kz');
+            $table->bigInteger('phone');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('add__projects');
+    }
+}
