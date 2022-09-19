07706fc70d9fdefaa1bb30fa9e40f6adc83fad0a
maksat <baubek.maksat@yandex.ru>
2022-09-19 11:50:08 +0600

    Fix ProjectController

--- src/app/Http/Controllers/API/V1/ProjectController.php
+++ src/app/Http/Controllers/API/V1/ProjectController.php
@@ -3,52 +3,55 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+
 use App\Http\Requests\V1\ProjectStoreRequest;
+use App\Http\Requests\V1\ProjectUpdateRequest;
+
 use App\Http\Resources\V1\ProjectResource;
+
+use App\Repositories\V1\RoleRepository;
+use App\Services\V1\ProjectService;
+
 use App\Models\Project;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Validator;
+use App\Services\V1\RoleService;
 
 class ProjectController extends Controller
 {
-    public function index()
+    protected $service;
+
+    public function __construct(ProjectService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
     {
         return response()->json(ProjectResource::collection(Project::all()), 200);
     }
 
-    public function show($id)
+    public function show(Project $project)
     {
-        return new ProjectResource(Project::findOrFail($id));
+        return new ProjectResource($project);
     }
 
     public function store(ProjectStoreRequest $request)
     {
+        $project = $this->service->store($request);
 
-        $request->validate([
-
-            'locale' => ['required', Rule::in(['kk', 'ru',]),],
-        ]);
-
-        Project::create($request->validated());
-
-
-        return response()->json(ProjectResource::collection(Project::all()), 201);
-
+        return response()->json(new ProjectResource($project), 201);
     }
 
     public function update(ProjectStoreRequest $request, Project $project)
     {
-        $project->update($request->validated());
+        $this->service->update($request, $project);
 
         return new ProjectResource($project);
-
     }
 
-    public function destroy(Project $project/*, ProjectStoreRequest $request*/)
+    public function destroy(Project $project)
     {
-        $project->delete();
+        $this->service->destroy($project);
 
-         return response()->json(null, 204);
+        return response()->json(null, 204);
     }
 }
