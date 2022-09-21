c88e4aab4c0e4770cce02e1490ac300ad4a61c53
Artem <alexx1088@gmail.com>
2022-09-21 20:38:53 +0600

    change rout

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\TestController::class, 'index']);
 
-Route::get('/testo', [\App\Http\Controllers\TestController::class, 'show'])
-    ->name('testo');
+Route::get('/test', [\App\Http\Controllers\TestController::class, 'show'])
+    ->name('test');
